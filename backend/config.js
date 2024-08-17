f1b5efb16b6fbbb97714b605762427009e61aa09
satishs <sshsatishs23@gmail.com>
2024-08-18 02:01:16 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT=1823;
+
+export const mongoDBURL=
+' mongodb+srv://PHARMAMATE:PHARMAMATE@cluster0.psukc.mongodb.net/PHARMAMATE?retryWrites=true&w=majority&appName=Cluster0'
+
---
+++ backend/index.js
@@ -0,0 +1,25 @@
+// #starting project file 
+import express from "express";
+import { PORT,mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+
+const app=express();
+
+app.get('/',(request,response) => {
+    console.log(request)
+    return response.status(234).send('hello')
+});
+
+app.listen(PORT,()=>{
+    console.log(`App is listeing to port :${PORT}`)
+})
+
+// to connect the  database
+mongoose
+ .connect(mongoDBURL) 
+ .then(() => {
+    console.log('Connected to database');
+ })
+ .catch((error) => {
+   console.log(error);   
+ });
